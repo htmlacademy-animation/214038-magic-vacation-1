e8bc51e02dc008b2ba3b5ef414f180e4d163903b
eperkhalsky <egor.perkhalsky@amasty.com>
2021-11-16 10:02:40 +0300

    4.12. Реализовываем анимацию сцены с крокодилом

--- source/js/modules/canvas/scene-2d.js
+++ source/js/modules/canvas/scene-2d.js
@@ -192,6 +192,9 @@ export default class Scene2D {
     if (isContextTransforming) {
       this.ctx.restore();
     }
+
+    object.widthImg = width;
+    object.heightImg = height;
   }
 
   clearScene() {
