eceb31848c84edbbd98e7124f00166093e9858aa
eperkhalsky <egor.perkhalsky@amasty.com>
2021-12-03 19:55:40 +0300

    [module5-task6] 5.39. Анимируем мигание интенсивности эффекта Hue

--- source/js/modules/three-js/story.js
+++ source/js/modules/three-js/story.js
@@ -2,6 +2,14 @@ import ThreeJsCanvas from "./three-js-canvas";
 import * as THREE from "three";
 import {storyRowShaderMaterial} from "./simple-raw-shader-material";
 
+let animationHueSettings = {
+  initialHue: 0,
+  finalHue: -0.4,
+  currentHue: 0,
+  duration: 1,
+  timeStart: -1
+};
+
 export default class Story extends ThreeJsCanvas {
   constructor(canvasId) {
     super({
@@ -9,10 +17,13 @@ export default class Story extends ThreeJsCanvas {
     });
 
     this.center = {x: this.width / 2, y: this.height / 2};
+    this.isActiveTwoScreen = false;
+    this.hueIteration = 0;
+    this.countIteration = 0;
 
     this.textures = [
       {src: `./img/module-5/scenes-textures/scene-1.png`, options: {hue: 0.0}},
-      {src: `./img/module-5/scenes-textures/scene-2.png`, options: {hue: -0.4, isMagnifier: true}},
+      {src: `./img/module-5/scenes-textures/scene-2.png`, options: {hue: 0.0, isMagnifier: true}},
       {src: `./img/module-5/scenes-textures/scene-3.png`, options: {hue: 0.0}},
       {src: `./img/module-5/scenes-textures/scene-4.png`, options: {hue: 0.0}}
     ];
@@ -68,12 +79,99 @@ export default class Story extends ThreeJsCanvas {
     return new THREE.RawShaderMaterial(storyRowShaderMaterial({
       map: {value: texture.src},
       options: {value: texture.options},
-      ...this.addBubble(index),
+      ...this.addBubble(index)
     }));
   }
 
   setScene(sceneID) {
     this.camera.position.x = this.textureWidth * sceneID;
     this.render();
+
+    // запускем функцию, где определяется что второй слайд и запускаются эффекты для второго слайда
+    this.actionSlideTwo(sceneID);
+  }
+
+  actionSlideTwo(sceneID) {
+    this.resetHueCycle();
+
+    if (sceneID === 1) {
+      this.isActiveTwoScreen = true;
+      this.animateHueShift();
+    } else {
+      this.isActiveTwoScreen = false;
+      this.countIteration = 0;
+      this.hueIteration = 0;
+      this.resetHueShift();
+      animationHueSettings.currentHue = 0;
+    }
+  }
+
+  // обнуляем параметры для запуска нового цикла мигания
+  resetHueCycle() {
+    animationHueSettings.duration = this.getRandomNumber(0.5, 1); // рандомная длительность одного мигания
+    animationHueSettings.timeStart = Date.now() * 0.001; // обнуляем время отсчета
+  }
+
+  // обнуляем hue
+  resetHueShift() {
+    this.textures[1].options.hue = animationHueSettings.initialHue;
+  }
+
+  animateHueShift() {
+    this.animate();
+
+    // запускам эффект hue второй и третий раз
+    if ((this.hueIteration === 1 || this.hueIteration === 2) && this.countIteration === 0) {
+      this.countIteration = 1;
+      animationHueSettings.currentHue = 0;
+
+      this.resetHueCycle();
+    }
+
+    // если активен второй слайдер, то гоняем действует requestAnimationFrame, если нет - отменяет его
+    if (this.isActiveTwoScreen) {
+      requestAnimationFrame(() => {
+        this.animateHueShift();
+        this.render();
+      });
+    } else {
+      cancelAnimationFrame(() => {
+        this.animateHueShift();
+        this.render();
+      });
+    }
+  }
+
+  animate() {
+    let hueValue = animationHueSettings.currentHue;
+
+    if (animationHueSettings.timeStart > 0) {
+      const t = Date.now() * 0.001 - animationHueSettings.timeStart;
+
+      // время превышено для данного цикла анимации
+      if (t >= animationHueSettings.duration) {
+        animationHueSettings.timeStart = -1;
+        this.hueIteration++;
+        this.countIteration = 0;
+      }
+
+      const progress = t / animationHueSettings.duration;
+
+      if (progress < 0.5) {
+        hueValue = (animationHueSettings.initialHue + progress * (animationHueSettings.finalHue - animationHueSettings.initialHue)) * 2;
+      } else if (progress > 0.5) {
+        hueValue = (animationHueSettings.finalHue + progress * (animationHueSettings.initialHue - animationHueSettings.finalHue)) * 2;
+      }
+    }
+
+    // обновляем значение hue для последующей передачи в шейдер
+    if (hueValue !== animationHueSettings.currentHue && this.isActiveTwoScreen) {
+      this.textures[1].options.hue = hueValue;
+      animationHueSettings.currentHue = hueValue;
+    }
+  }
+
+  getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
   }
 }
